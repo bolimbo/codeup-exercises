08158d5db029e9b0cb277e01a4db8a4f37838001
Bolimbo <carlo.cappellini@yahoo.com>
2017-10-16 11:54:41 -0500

    solve String Utils Exercises

--- src/main/java/com/codeup/App.java
+++ src/main/java/com/codeup/App.java
@@ -1,13 +1,43 @@
 package com.codeup;
 
+// Java program to ReverseString using StringBuilder
+import org.apache.commons.lang3.StringUtils;
+
+import java.lang.*;
+import java.io.*;
+import java.util.*;
+
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App
+
 {
     public static void main( String[] args )
     {
+
+
+        Scanner scan = new Scanner(System.in);
         System.out.println( "Hello World!" );
+
+
+
+        System.out.println("enter something");
+
+        String line = scan.nextLine();
+
+        if (StringUtils.isNumeric(line)) {
+            System.out.println(line + " is a number");
+        } else {
+            System.out.println(line + " is not a number");
+            System.out.println("flipped case " + StringUtils.swapCase(line));
+            System.out.println("reversed " + StringUtils.reverse(line));
+        }
+
+
+
     }
 }
